f0d19b16f13ebc59116714e177496a1264174ee7
hcheong <hao.cheong@outlook.com>
2023-10-17 08:47:07 +1100

    Prepared setup for unit testing

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,86 +1,147 @@
-PETS = [
-    {
-        "name":"Pickles",
-        "age": 2
-    },
-    {
-        "name":"Rosie",
-        "age": 1
-    },
-    {
-        "name":"Abbie",
-        "age": 4
-    },
-    {
-        "name":"Cooper",
-        "age": 3
-    }
-]
+from app.database import Base
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from fastapi.testclient import TestClient
+import pytest
+from app.main import get_db
+from app.main import app
+import os
+import sys
+from datetime import datetime
 
-OWNERS = [
-    {
-        "name": "Alice",
-        "email": "alice@bigpond.com",
-        "home_address": "Unit 1, 124 Copernicus Avenue",
-        "password": "iLovePuppies123!"
-    },
-    {
-        "name": "Bob",
-        "email": "bob@ymail.com",
-        "home_address": "912 Dylan Lane",
-        "password": "pattingGiver381"
-    }
-]
+import pathlib
+ 
 
-TRAINERS = [
-    {
-        "name": "Eddie Bark",
-        "description": "Export in pet cardio",
-        "phone_no": "04442123123",
-        "email": "eddie.bark@plusWoofers.com",
-        "date_started": "2021-01-07T00:00:00.000Z",
-        "trainer_id": "TR-013",
-    },
-    {
-        "name": "Lara Meowstein",
-        "description": "Professional pet bodybuilding coach",
-        "phone_no": "0333789789",
-        "email": "lara.meowstein@plusWoofers.com",
-        "date_started": "2019-12-31T00:00:00.000Z",
-        "trainer_id": "TR-047",
-    }
-]
 
-NUTRITION_PLANS = [
-    {
-        "name": "Pickles Meal Deal",
-        "description": "High vitamin meal with low carbs",
-        "meal": {
-            "protein": "Chicken",
-            "carb": "Brown Rice",
-            "fibre": "Broccolli"
+ABS_PATH = pathlib.Path().resolve()
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{ABS_PATH}/app/db/pets.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+SUCCESS = 200
+ERROR = 400
+
+
+@ pytest.fixture
+def reset_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+@ pytest.fixture
+def owners_data():
+    return [
+        {
+            "name": "Alice",
+            "email": "alice@bigpond.com",
+            "home_address": "Unit 1, 124 Copernicus Avenue",
+            "password": "iLovePuppies123!"
         },
-        "starting_date": "2023-10-10T00:00:00.000Z",
-    },
-    {
-        "name": "Rosie Roast Plan",
-        "description": "High protein for the aspiring competitor",
-        "meal": {
-            "protein": "Beef",
-            "carb": "Dog Pasta",
-            "fibre": "Beans"
-        },
-        "starting_date": "2023-08-23T00:00:00.000Z",
-    },
-    {
-        "name": "Cooper Carb Load",
-        "description": "Dense carb plan for a picky eater",
-        "meal": {
-            "protein": "Fish",
-            "carb": "Puppy Penne",
-            "fibre": "Corn"
-        },
-        "starting_date": "2021-03-03T00:00:00.000Z",
-    },
+        {
+            "name": "Bob",
+            "email": "bob@ymail.com",
+            "home_address": "912 Dylan Lane",
+            "password": "pattingGiver381"
+        }
+    ]
 
-]
+@ pytest.fixture
+def trainers_data():
+    return [
+        {
+            "name": "Eddie Bark",
+            "description": "Export in pet cardio",
+            "phone_no": "04442123123",
+            "email": "eddie.bark@plusWoofers.com",
+            "date_started": "2021-01-07T00:00:00.000Z",
+            "trainer_id": "TR-013",
+        },
+        {
+            "name": "Lara Meowstein",
+            "description": "Professional pet bodybuilding coach",
+            "phone_no": "0333789789",
+            "email": "lara.meowstein@plusWoofers.com",
+            "date_started": "2019-12-31T00:00:00.000Z",
+            "trainer_id": "TR-047",
+        }
+    ]
+
+@ pytest.fixture
+def pets_data():
+    return [
+        {
+            "name":"Pickles",
+            "age": 2
+        },
+        {
+            "name":"Rosie",
+            "age": 1
+        },
+        {
+            "name":"Abbie",
+            "age": 4
+        },
+        {
+            "name":"Cooper",
+            "age": 3
+        }
+    ]
+
+@ pytest.fixture
+def nutrition_plans_data():
+    return [
+        {
+            "name": "Pickles Meal Deal",
+            "description": "High vitamin meal with low carbs",
+            "meal": {
+                "protein": "Chicken",
+                "carb": "Brown Rice",
+                "fibre": "Broccolli"
+            },
+            "starting_date": "2023-10-10T00:00:00.000Z",
+        },
+        {
+            "name": "Rosie Roast Plan",
+            "description": "High protein for the aspiring competitor",
+            "meal": {
+                "protein": "Beef",
+                "carb": "Dog Pasta",
+                "fibre": "Beans"
+            },
+            "starting_date": "2023-08-23T00:00:00.000Z",
+        },
+        {
+            "name": "Cooper Carb Load",
+            "description": "Dense carb plan for a picky eater",
+            "meal": {
+                "protein": "Fish",
+                "carb": "Puppy Penne",
+                "fibre": "Corn"
+            },
+            "starting_date": "2021-03-03T00:00:00.000Z",
+        },
+
+    ]
+
