35287cddea199316d42fe2db1dfdc64f423a99f8
HaoCheong <61342120+HaoCheong@users.noreply.github.com>
2023-10-18 14:56:00 +1100

    Added comments for trainer crud test

--- tests/unit/trainer_test.py
+++ tests/unit/trainer_test.py
@@ -1,10 +1,18 @@
+'''trainer_test.py
+
+Unit tests for each Trainer Crud Endpoint
+
+'''
+
 from unit.conftest import client, SUCCESS, ERROR
 from unit import wrappers
 
 def test_create_trainer(reset_db, trainers_data):
+    ''' Testing the success case of creating an trainer '''
     assert wrappers.create_trainer(trainers_data[0])['status'] == SUCCESS
 
 def test_get_all_trainer(reset_db, trainers_data):
+    ''' Testing the success case of getting all trainers '''
     trainers = [wrappers.create_trainer(trainers_data[i])
                 for i in range(0, len(trainers_data))]
 
@@ -15,23 +23,26 @@ def test_get_all_trainer(reset_db, trainers_data):
     assert len(trainers) == len(all_trainers)
 
 def test_get_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the success case of getting specified trainer '''
     trainer = wrappers.create_trainer(trainers_data[0])['data']
     ret_trainer = wrappers.get_trainer_by_trainer_id(trainer['trainer_id'])['data']
     assert trainer["name"] == ret_trainer["name"]
 
 def test_invalid_get_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the failing case of getting specified trainer '''
     trainer = wrappers.create_trainer(trainers_data[0])['data']
     ret_trainer = wrappers.get_trainer_by_trainer_id("TR-000")
     assert ret_trainer['status'] == ERROR
 
 def test_delete_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the success case of deleting trainer '''
     trainer = wrappers.create_trainer(trainers_data[0])['data']
     
     # Check pre-delete statue
     pre_check_res = wrappers.get_trainer_by_trainer_id(trainer['trainer_id'])
     assert pre_check_res['status'] == SUCCESS
 
-    # Check deletion event
+    # Check deletion request status
     delete_res = wrappers.delete_trainer_by_trainer_id(trainer['trainer_id'])
     assert delete_res['status'] == SUCCESS
 
@@ -40,13 +51,15 @@ def test_delete_trainer_by_trainer_id(reset_db, trainers_data):
     assert post_check_res['status'] == ERROR
 
 def test_invalid_delete_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the fail case of deleting trainer '''
+
     trainer = wrappers.create_trainer(trainers_data[0])['data']
 
     # Check pre-delete statue
     pre_check_res = wrappers.get_trainer_by_trainer_id(trainer['trainer_id'])
     assert pre_check_res['status'] == SUCCESS
 
-    # Check deletion event
+    # Check deletion request status, with invalid ID provided
     delete_res = wrappers.delete_trainer_by_trainer_id("TR-000")
     assert delete_res['status'] == ERROR
 
@@ -55,24 +68,30 @@ def test_invalid_delete_trainer_by_trainer_id(reset_db, trainers_data):
     assert post_check_res['status'] == SUCCESS
 
 def test_update_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the success case of updating trainer '''
 
     # Checks that both trainers data are identical
     trainer = wrappers.create_trainer(trainers_data[0])['data']
     new_trainer = trainers_data[1]
     assert trainer['name'] != new_trainer['name']
 
+    # Checks update response status is correct
     new_trainer = trainers_data[1]
     update_trainer = wrappers.update_trainer_by_trainer_id(trainer['trainer_id'], new_trainer)
     assert update_trainer['status'] == SUCCESS
 
+    # Check the update values are correct
     assert update_trainer['data']['name'] == new_trainer['name']
 
 def test_invalid_update_trainer_by_trainer_id(reset_db, trainers_data):
+    ''' Testing the fail case of updating trainer '''
+
+    # Checks update response status is invalid, from invalid ID provided
     trainer = wrappers.create_trainer(trainers_data[0])['data']
     new_trainer = trainers_data[1]
     update_trainer = wrappers.update_trainer_by_trainer_id("TR-000", new_trainer)
-
     assert update_trainer['status'] == ERROR
 
-    post_check_res = wrappers.get_trainer_by_trainer_id(trainer['trainer_id'])
-    assert post_check_res['status'] == SUCCESS
+    # Checks that the current trainer is untouched
+    curr_trainer = wrappers.get_trainer_by_trainer_id(trainer['trainer_id'])
+    assert curr_trainer['data']['name'] == trainer['name']
