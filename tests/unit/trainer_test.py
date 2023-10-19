ec1aa457d51c41c5a0215e6441d239552641e149
hcheong <hao.cheong@outlook.com>
2023-10-19 11:02:03 +1100

    Adjusted commenting on the test files

--- tests/unit/trainer_test.py
+++ tests/unit/trainer_test.py
@@ -2,6 +2,14 @@
 
 Unit tests for each Trainer Crud Endpoint
 
+Each tests also have a reset_db fixture function. Clears the database after every test.
+
+Certain test may require us to redo requests to setup the appropriate circumstance to test, we
+do not re-assert their values as we assume they are correct based on previous.
+
+Testing should always validate 2 things:
+ - Validated the response status correctness
+ - Validate the data correctness
 '''
 
 from unit.conftest import client, SUCCESS, ERROR
