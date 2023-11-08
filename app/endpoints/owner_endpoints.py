a612b5f7b03105d59f073ec77bd2f1f50ac116e1
HaoCheong <61342120+HaoCheong@users.noreply.github.com>
2023-11-08 14:21:11 +1100

    Fixed up incorrect schema usage

--- app/endpoints/owner_endpoints.py
+++ app/endpoints/owner_endpoints.py
@@ -22,7 +22,7 @@ def get_all_owners(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
     return db_owners
 
 
-@router.get("/owner/{owner_id}", response_model=schemas.OwnerReadNR, tags=["Owners"])
+@router.get("/owner/{owner_id}", response_model=schemas.OwnerReadWR, tags=["Owners"])
 def get_owner_by_id(owner_id: int, db: Session = Depends(get_db)):
     db_owner = cruds.get_owner_by_id(db, id=owner_id)
     if not db_owner:
@@ -32,7 +32,7 @@ def get_owner_by_id(owner_id: int, db: Session = Depends(get_db)):
 
 
 @router.patch("/owner/{owner_id}", response_model=schemas.OwnerReadNR, tags=["Owners"])
-def update_owner_by_id(owner_id: int, new_owner: schemas.OwnerReadNR, db: Session = Depends(get_db)):
+def update_owner_by_id(owner_id: int, new_owner: schemas.OwnerUpdate, db: Session = Depends(get_db)):
     db_owner = cruds.get_owner_by_id(db, id=owner_id)
     if not db_owner:
         raise HTTPException(status_code=400, detail="Owner does not exist")
