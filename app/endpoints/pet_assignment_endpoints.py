130dc4502f8080241b4973ecdff1a7f7077c39fd
hcheong <hao.cheong@outlook.com>
2023-11-11 17:15:01 +1100

    Fixed up pet_assignment spacing

--- app/endpoints/pet_assignment_endpoints.py
+++ app/endpoints/pet_assignment_endpoints.py
@@ -49,8 +49,8 @@ def unassign_pet_from_owner(pet_id: int, owner_id: int, db: Session = Depends(ge
 
     return pet_assignment_cruds.unassign_pet_from_owner(db, pet_id=pet_id, owner_id=owner_id)
 
-# ======== ASSIGNING PETS TO TRAINER ========
 
+# ======== ASSIGNING PETS TO TRAINER ========
 
 @router.post("/assignToTrainer/{pet_id}/{trainer_id}", tags=["Item Assignments"])
 def assign_pet_to_trainer(pet_id: int, trainer_id: str, db: Session = Depends(get_db)):
@@ -91,6 +91,7 @@ def unassign_pet_from_trainer(pet_id: int, trainer_id: str, db: Session = Depend
 
     return pet_assignment_cruds.unassign_pet_from_trainer(db, pet_id=pet_id, trainer_id=trainer_id)
 
+
 @router.post("/assignToNutritionPlan/{pet_id}/{nutrition_plan_id}", tags=["Item Assignments"])
 def assign_pet_to_nutrition_plan(pet_id: int, nutrition_plan_id: int, db: Session = Depends(get_db)):
     db_pet = pet_cruds.get_pet_by_id(db, id=pet_id)
@@ -110,6 +111,7 @@ def assign_pet_to_nutrition_plan(pet_id: int, nutrition_plan_id: int, db: Sessio
             status_code=400, detail="Pet already assigned to nutritional plan")
     
     return pet_assignment_cruds.assign_pet_to_nutrition_plan(db, pet_id=pet_id, nutrition_plan_id=nutrition_plan_id)
+
 
 @router.post("/unassignFromNutritionPlan/{pet_id}", tags=["Item Assignments"])
 def unassign_pet_from_nutrition_plan(pet_id: int, db: Session = Depends(get_db)):
