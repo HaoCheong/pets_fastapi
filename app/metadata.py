f1219b386f75faab3f102952dcbe803d6e7f65a9
hao.cheong <hao.cheong@kgit.com.au>
2025-04-28 10:28:22 +1000

    Removed old, move out new, fixed up dump file errors

---
+++ app/metadata.py
@@ -0,0 +1,33 @@
+# The tags for the FastAPI OpenAPI auto documenter
+tags_metadata = [
+    {
+        "name": "Pets",
+        "description": "Operations with Pets",
+    },
+    {
+        "name": "Owners",
+        "description": "Operations with Pet Owners",
+    },
+    {
+        "name": "Nutrition Plans",
+        "description": "Operations with Nutrition Plan",
+    },
+    {
+        "name": "Trainers",
+        "description": "Operations with Pet Trainers",
+    },
+    {
+        "name": "Item Assignments",
+        "description": "Operations with that assign pets to trainers, owners, or nutrition plans",
+    }
+]
+
+swagger_ui_parameters = {
+    "syntaxHighlight": True
+}
+
+app_title = 'Pets Sample v1.2 FastAPI'
+app_version = '1.2.0'
+app_desc = '''
+This is the FastAPI Sample Project. Designed to NOT use SQLModel
+'''
